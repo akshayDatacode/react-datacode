ce0d83069b9237710b1083f0ec1c48e7ccc5ab5e
akshayDatacode <akshay.webllisto@gmail.com>
2020-03-25 17:22:57 +0530

    Quize Logic Improvment and Error Solved

--- src/components/main_component/posts/mcqQuestions/PythonQuize.jsx
+++ src/components/main_component/posts/mcqQuestions/PythonQuize.jsx
@@ -1,63 +1,66 @@
-import React, { Component } from 'react';
-import Quize from './quize';
+import React, { Component } from "react";
+import Quize from "./quize";
 class PythonQuize extends Component {
-    state = { 
-        isCorrect : false,
-        isClicked : false,
-        count : 0,
-        status : ''
-     }
-    
-    counter = (count) =>{
-         this.setState({ count : this.state.count + 1 });
-    }
+  state = {
+    isCorrect: false,
+    isClicked: false,
+    count: 0,
+    status: ""
+  };
 
-    render() { 
-        return ( 
-            <>
-            <h1>Python Quize</h1>
-            <div className="row">
-                <div className=" border border-success m-2 col">
-                    Total Correct :- <span className="badge badge-primary" > {this.state.count}  </span>
-                    <Quize 
-                    question = "What is Python"
-                    correctAnswer = "Programming"
-                    v1 = "Programming"
-                    v2 = "Azgar"
-                    v3 = "Language"
-                    counter = {this.counter}
-                    />
-                    <Quize 
-                    question = "What is Akshay"
-                    correctAnswer = "Speaker"
-                    v1 = "Student"
-                    v2 = "Developer"
-                    v3 = "Speaker"
-                    counter = {this.counter}
-                    />
-                    <Quize 
-                    question = "Where Akshay Live"
-                    correctAnswer = "Mandsaur"
-                    v1 = "NewYork"
-                    v2 = "Indore"
-                    v3 = "Mandsaur"
-                    counter = {this.counter}
-                    />
-                    <Quize 
-                    question = "Did you Liked Datacode"
-                    correctAnswer = "Yes"
-                    v1 = "No"
-                    v2 = "Yes"
-                    v3 = "MayBe"
-                    counter = {this.counter}
-                    />
-                </div>
+  counter = count => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
-            </div>
-
-            </>
-         );
-    }
+  render() {
+    return (
+      <>
+        <h1>Python Quize</h1>
+        <div className="row">
+          <div className=" border border-success m-2 col">
+            Total Correct :-{""}
+            <span className="badge badge-primary"> {this.state.count} </span>
+            <Quize
+              quizeNo="1"
+              question="What is Python"
+              correctAnswer="Programming"
+              v1="Programming"
+              v2="Azgar"
+              v3="Language"
+              counter={this.counter}
+            />
+            <Quize
+              quizeNo="2"
+              question="What is Akshay"
+              correctAnswer="Speaker"
+              v1="Student"
+              v2="Developer"
+              v3="Speaker"
+              counter={this.counter}
+            />
+            <Quize
+              quizeNo="3"
+              question="Where Akshay Live"
+              correctAnswer="Mandsaur"
+              v1="NewYork"
+              v2="Indore"
+              v3="Mandsaur"
+              counter={this.counter}
+            />
+            <Quize
+              quizeNo="4"
+              question="Did you Liked Datacode"
+              correctAnswer="Yes"
+              v1="No"
+              v2="Yes"
+              v3="MayBe"
+              counter={this.counter}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
- 
+
 export default PythonQuize;
--- src/components/main_component/posts/mcqQuestions/quize.js
+++ src/components/main_component/posts/mcqQuestions/quize.js
@@ -1,48 +1,66 @@
-import React, { Component } from 'react';
-class Quize  extends Component {
-    state = { 
-        isClicked : false,
-        status : ""
-     }
+import React, { Component } from "react";
+class Quize extends Component {
+  state = {
+    isClicked: false,
+    status: ""
+  };
 
-     handleOption = (event) =>{
-        if(event.target.value === this.props.correctAnswer) {
-        this.setState({ isClicked : true , status : "correct"})
-        this.props.counter()
-        }else{
-            this.setState({ isClicked : true , status : "wrrong"})
-        }
-     }
-     getColorChange = () => {
-        let classes = "badge m-2 badge-";
-        if (this.state.status === "correct") {
-          classes += "success";
-        } 
-        if(this.state.status === 'wrrong'){
-          classes += "danger";
-        }
-        return classes;
-      };
-    render() { 
-        return ( 
-            <>
-            <h6>Quize : 1</h6>  
-                    <h4>{this.props.question}</h4>
-                    {!this.state.status === "" && (
-                        <span className={this.getColorChange()}> {this.state.status}</span>
-                    )}
-                     <br/>
-                    {!this.state.isClicked && (  
-                    <>    
-                    <h5>Options:</h5>
-                    <input type="radio" name="op1" value={this.props.v1} onClick={this.handleOption} /> {this.props.v1} <br/>
-                    <input type="radio" name="op1" value={this.props.v2} onClick={this.handleOption} /> {this.props.v2} <br/> 
-                    <input type="radio" name="op1" value={this.props.v3} onClick={this.handleOption}/> {this.props.v3}
-                    </>
-                    )}
-            </>
-         );
+  handleOption = event => {
+    if (event.target.value == this.props.correctAnswer) {
+      this.setState({ isClicked: true, status: "correct" });
+      this.props.counter();
+    } else {
+      this.setState({ isClicked: true, status: "wrrong" });
     }
+  };
+  getColorChange = () => {
+    let classes = "badge m-2 badge-";
+    if (this.state.status == "correct") {
+      classes = classes + "success";
+    }
+    if (this.state.status == "wrrong") {
+      classes = classes + "danger";
+    }
+    return classes;
+  };
+  render() {
+    return (
+      <>
+        <h6>Quize : {this.props.quizeNo}</h6>
+        <h4>{this.props.question}</h4>
+        {!this.state.status == "" && (
+          <span className={this.getColorChange()}> {this.state.status}</span>
+        )}
+        <br />
+        {!this.state.isClicked && (
+          <>
+            <h5>Options:</h5>
+            <input
+              type="radio"
+              name="op1"
+              value={this.props.v1}
+              onClick={this.handleOption}
+            />{" "}
+            {this.props.v1} <br />
+            <input
+              type="radio"
+              name="op1"
+              value={this.props.v2}
+              onClick={this.handleOption}
+            />{" "}
+            {this.props.v2} <br />
+            <input
+              type="radio"
+              name="op1"
+              value={this.props.v3}
+              onClick={this.handleOption}
+            />{" "}
+            {this.props.v3}
+          </>
+        )}
+      </>
+    );
+  }
 }
- 
-export default Quize ;
+
+export default Quize;
