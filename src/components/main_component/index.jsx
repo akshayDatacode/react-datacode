142b347247491dcdebfcb3218a7f730a42a756ec
akshayDatacode <akshay.webllisto@gmail.com>
2020-03-27 17:57:40 +0530

    LAnding Page Improvment with Index File Position changes

--- src/components/main_component/index.jsx
+++ src/components/main_component/index.jsx
@@ -9,9 +9,10 @@ import {
   faCodepen,
   faDashcube,
   faHtml5,
-  faJava
- 
+  faJava,
+  faFirstOrder
 } from "@fortawesome/free-brands-svg-icons";
+import { faChessQueen } from "@fortawesome/free-solid-svg-icons";
 
 class MainComponent extends Component {
   state = {};
@@ -33,189 +34,71 @@ class MainComponent extends Component {
             </center>
           </div>
         </div>
-        <hr/>
-        <br/>
-<br/> 
 
-        <div className="container">
-				<div class="row">
-					<div class="col-lg-12 ">
-						<div class="row">
-							<div class="col-sm-3 col-md-3 col-lg-3 ">
-									<div class="aligncenter">
-										<div class="icon">
-                    <a href="https://www.facebook.com/Datacode.in/" cl>
-                    <FontAwesomeIcon
-                      icon={faCodepen}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-                  </a>								
-                  		</div>
-										<h6>Fully responsive</h6>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3 col-lg-3">
-								<div class="box">
-									<div class="aligncenter">
-										<div class="icon">
-                    <FontAwesomeIcon
-                      icon={faDashcube}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-                 																</div>
-										<h6>Fully responsive</h6>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3 col-lg-3">
-								<div class="box">
-									<div class="aligncenter">
-										<div class="icon">
-                    <FontAwesomeIcon
-                      icon={faHtml5}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-             																    </div>
-										<h6>Fully responsive</h6>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3 col-lg-3 ">
-								<div class="box">
-									<div class="aligncenter">
-										<div class="icon">
-                    <FontAwesomeIcon
-                      icon={faJava}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-                  										</div>
-										<h6>Fully responsive</h6>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-      <br/>
-<br/> 
-     <hr/>
-     <br/>
-<br/> 
-<br/>
+        <div className="container-fluid border border-primary">
+          <div class="row m-5">
+            <div class="col-md-3 ml-5 mr-5">
+              <div className="card-fluid bg-light shadow rounded m-3 pt-3">
+                <center>
+                  <FontAwesomeIcon icon={faCodepen} size="10x" />
+                </center>
+                <div className="card-body">
+                  <h5 className="card-title text-info">Code Pen</h5>
+                  <p clsssName=" card-text text-light ">
+                    We Usually want to make a site for just fun and timepass
+                  </p>
+                </div>
 
+                <div className="row">
+                  <div className="col-6"></div>
+                  <div className="col-5 mb-2  pr-2">
+                    <a href="/home">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 ml-5 mr-5">
+              <div className="card-fluid bg-light shadow rounded m-3 pt-3">
+                <center>
+                  <FontAwesomeIcon icon={faFirstOrder} size="10x" />
+                </center>
+                <div className="card-body">
+                  <h5 className="card-title text-info">Code Pen</h5>
+                  <p clsssName=" card-text text-light ">
+                    We Usually want to make a site for just fun and timepass
+                  </p>
+                </div>
 
+                <div className="row">
+                  <div className="col-6"></div>
+                  <div className="col-5 mb-2 pr-2">
+                    <a href="/home">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 ml-5 mr-5">
+              <div className="card-fluid bg-light shadow rounded m-3 pt-3">
+                <center>
+                  <FontAwesomeIcon icon={faChessQueen} size="10x" />
+                </center>
+                <div className="card-body">
+                  <h5 className="card-title text-info">Datacode Kings</h5>
+                  <p clsssName=" card-text text-light ">
+                    We Usually want to make a site for just fun and timepass
+                  </p>
+                </div>
 
-      <div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="row">
-							<div class="col-sm-6 col-lg-6">
-								<h4>About our company</h4>
-								<p><strong>Meliore inciderint qui ne. Suas cotidieque vel ut lobortis reformidans duo</strong></p>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exercitationem praesentium deleniti nostrum laborum rem id nihil tempora. Adipisci ea commodi unde nam placeat cupiditate quasi a ducimus rem consequuntur ex eligendi minima
-									voluptatem assumenda voluptas quidem sit maiores odio velit voluptate
-								</p>
-								<p>
-									Mel explicari adipiscing consectetuer no, no mel apeirian scripserit repudiandae, ad assum mundi scribentur eam. Graecis offendit phaedrum eu his, eius ferri quidam eos ad, quis delenit vel ei. Alia modus facete te eos, eu tation appellantur per
-								</p>
-							</div>
-							<div class="col-sm-6 col-lg-6">
-								<h4>this block is for image</h4>
-						
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-      <hr/>
-        <br/>
-<br/> 
-
-        <div className="container">
-				<div class="row">
-					<div class="col-lg-12 ">
-						<div class="row">
-							<div class="col-sm-3 col-md-3 col-lg-3 ">
-									<div class="aligncenter">
-										<div class="icon">
-                    <FontAwesomeIcon
-                      icon={faCodepen}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-                  		</div>
-										<h6>Fully responsive</h6>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3 col-lg-3">
-								<div class="box">
-									<div class="aligncenter">
-										<div class="icon">
-                    <FontAwesomeIcon
-                      icon={faDashcube}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-                  																</div>
-										<h6>Fully responsive</h6>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3 col-lg-3">
-								<div class="box">
-									<div class="aligncenter">
-										<div class="icon">
-                    <FontAwesomeIcon
-                      icon={faHtml5}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-                  															</div>
-										<h6>Fully responsive</h6>
-									</div>
-								</div>
-							</div>
-							<div class="col-sm-3 col-md-3 col-lg-3 ">
-								<div class="box">
-									<div class="aligncenter">
-										<div class="icon">
-                    <FontAwesomeIcon
-                      icon={faJava}
-                      size="5x"
-                      color="black"
-                      className="m-2"
-                    />
-                											</div>
-										<h6>Fully responsive</h6>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-      <br/>
-<br/> 
-     <hr/>
-     <br/>
-<br/> 
-<br/>
-             
-      
+                <div className="row">
+                  <div className="col-6"></div>
+                  <div className="col-5 mb-2 pr-2">
+                    <a href="/home">Read More</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
