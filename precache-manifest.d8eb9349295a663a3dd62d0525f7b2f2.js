a883e32c9ff1fef1553be721f0bc6c976f1db473
akshayDatacode <akshay.webllisto@gmail.com>
2020-03-27 23:37:34 +0530

    Updates

---
+++ precache-manifest.d8eb9349295a663a3dd62d0525f7b2f2.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88025a8444e919c84b1d2b5bf1fd52ea",
+    "url": "/react-datacode.in/index.html"
+  },
+  {
+    "revision": "4645ee64b2342ff506f2",
+    "url": "/react-datacode.in/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e2a4314c5ec65ba9d377",
+    "url": "/react-datacode.in/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4645ee64b2342ff506f2",
+    "url": "/react-datacode.in/static/js/2.1d598a48.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/react-datacode.in/static/js/2.1d598a48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a4314c5ec65ba9d377",
+    "url": "/react-datacode.in/static/js/main.0ae21394.chunk.js"
+  },
+  {
+    "revision": "5fdec35ade7f6236807f",
+    "url": "/react-datacode.in/static/js/runtime-main.eb019ca3.js"
+  },
+  {
+    "revision": "6bc137afee803aec9b9daa3cfbbcc417",
+    "url": "/react-datacode.in/static/media/Cover Photo (3).6bc137af.png"
+  },
+  {
+    "revision": "39eabcb71339d36994fb545af35d2c9c",
+    "url": "/react-datacode.in/static/media/Shootpy_W Cover Page (1).39eabcb7.jpg"
+  },
+  {
+    "revision": "17a6aab685d1ff492666d9c503f8579a",
+    "url": "/react-datacode.in/static/media/coder1.17a6aab6.jpeg"
+  },
+  {
+    "revision": "84a2e6ad6a4a98c2f6d49243b054681b",
+    "url": "/react-datacode.in/static/media/dbms_interview_question.jpeg.84a2e6ad.jpeg"
+  },
+  {
+    "revision": "78cb0203ba5f313e0292e7ee1735e3d1",
+    "url": "/react-datacode.in/static/media/ds1.jpg.78cb0203.png"
+  },
+  {
+    "revision": "e03defd019c51307e7db7aa9f0a9affc",
+    "url": "/react-datacode.in/static/media/h2.e03defd0.jpeg"
+  },
+  {
+    "revision": "014ae370995a1aaa6fe99791384bc67f",
+    "url": "/react-datacode.in/static/media/header2.014ae370.jpeg"
+  },
+  {
+    "revision": "7527ed34633b64b7f68b9a95e1d7cd58",
+    "url": "/react-datacode.in/static/media/headermain.7527ed34.jpeg"
+  },
+  {
+    "revision": "c2d14b11dc654d3e4eac809598d6244f",
+    "url": "/react-datacode.in/static/media/oops1.jpeg.c2d14b11.png"
+  },
+  {
+    "revision": "429f9e69c23535a032da5438e12daf81",
+    "url": "/react-datacode.in/static/media/pythonmcq.429f9e69.jpeg"
+  },
+  {
+    "revision": "48a0452102e4c880fa143ecb658cb416",
+    "url": "/react-datacode.in/static/media/pythontech.48a04521.jpeg"
+  },
+  {
+    "revision": "4e71cc54dec3312b3d8b02c83028322d",
+    "url": "/react-datacode.in/static/media/sql1.4e71cc54.jpeg"
+  },
+  {
+    "revision": "ae6a7be82eb8f079f86e90d7df399ba2",
+    "url": "/react-datacode.in/static/media/tech.ae6a7be8.png"
+  },
+  {
+    "revision": "6477a0de9e6ec8a1714495af71bfbb12",
+    "url": "/react-datacode.in/static/media/webcover1.6477a0de.jpeg"
+  },
+  {
+    "revision": "c11e0a5814e9762ad49e029e44ebd39f",
+    "url": "/react-datacode.in/static/media/webcover2.c11e0a58.jpeg"
+  },
+  {
+    "revision": "f49f705f2d4f07281cab880962a8667b",
+    "url": "/react-datacode.in/static/media/webcover3.f49f705f.jpeg"
+  }
+]);
