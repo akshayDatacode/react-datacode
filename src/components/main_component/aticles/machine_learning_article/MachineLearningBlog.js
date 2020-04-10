1d4cfaec6e3e502e51c4bf784a60ed9595036570
dhanwebllisto <dhanshreewebllisto01@gmail.com>
2020-04-10 22:01:23 +0530

    design changes

--- src/components/main_component/aticles/machine_learning_article/MachineLearningBlog.js
+++ src/components/main_component/aticles/machine_learning_article/MachineLearningBlog.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import HeaderBannerSection from "../../HeaderBannerSection";
 import RightSideSection from "../../../right_section/RightSideSection";
 import PostCard from "../../PostCard";
+import ArticlePostCard from "../ArticlePostCard";
+
 import { Link } from "react-router-dom";
 
 class MachineLearningBlog extends Component {
@@ -12,92 +14,49 @@ class MachineLearningBlog extends Component {
   render() {
     return (
       <>
-        <div className="container-fluid ">
+       <div className="container-fluid ">
           <div className="row">
             <div className="col-md-1 p-0 m-0"></div>
             <div className="col-md-8 p-0 mt-4">
-              <h2 className="text-center mb-4">
-                Machine Learning Community Blogs
-              </h2>
+              <h2 className="text-center mb-4">Programming Community Blogs</h2>
               <Link to="/home" className="badge badge-primary mr-1">
                 Home >
               </Link>
               <Link to="/index_articles" className="badge badge-warning mr-1">
                 Blogs >
               </Link>
+
               <div className="card-deck m-2">
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Programming Languages Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/prog_lan_blog"
+                <ArticlePostCard
+                  imgsource="js.jpeg"
+                  tag1="JavaScript"
+                  tag2="programming"
+                  title="Learn JavaScript concepts by building a random quote generator"
+                  paragraph="Learning JavaScript is fun. But What's the point if you are not building stuff with it. So in this article,"
+                  link="https://thenextbigwriter.tech/learn-javascript-concepts-by-building-a-random-quote-generator-ck8fmvz6j014qyys1i204knru"
                 />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
+                <ArticlePostCard
+                  imgsource="js.jpeg"
+                  tag1="JavaScript"
+                  tag2="methods"
+                  title="What is `this` inside foo.bar()?"
+                  paragraph="Calling a method directly works as expected.
+                  But when we pass a method as callback, it loses"
+                  link="https://blog.bendtherul.es/what-is-this-inside-foobar-ck8dzlitm01atxjs1322jz9a2"
                 />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
+                <ArticlePostCard
+                  imgsource="js.jpeg"
+                  tag1="python"
+                  tag2="python 101"
+                  title="Python 101 – Working with Strings"
+                  paragraph="You will be using strings very often when you program. A string is a series of letters surrounded by single, double or triple quotes."
+                  link="https://www.blog.pythonlibrary.org/2020/04/07/python-101-working-with-strings/"
                 />
               </div>
-              <div className="card-deck m-2">
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Programming Languages Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/prog_lan_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
               </div>
-              <div className="card-deck m-2">
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Programming Languages Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/prog_lan_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
-              </div>
-            </div>
             <div className="col-md-3 ">
               <RightSideSection />
-            </div>
+              </div>
           </div>
         </div>
       </>
