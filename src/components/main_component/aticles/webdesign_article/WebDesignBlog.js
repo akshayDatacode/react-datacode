1f78e6541c74ea78561e79978c7f572577b65eea
dhanwebllisto <dhanshreewebllisto01@gmail.com>
2020-04-10 22:03:15 +0530

    design changes and articles added

--- src/components/main_component/aticles/webdesign_article/WebDesignBlog.js
+++ src/components/main_component/aticles/webdesign_article/WebDesignBlog.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import HeaderBannerSection from "../../HeaderBannerSection";
 import RightSideSection from "../../../right_section/RightSideSection";
 import PostCard from "../../PostCard";
+import ArticlePostCard from "../ArticlePostCard";
+
 import { Link } from "react-router-dom";
 
 class ProgrammingLanguagesBlog extends Component {
@@ -12,90 +14,48 @@ class ProgrammingLanguagesBlog extends Component {
   render() {
     return (
       <>
-        <div className="container-fluid ">
+       <div className="container-fluid ">
           <div className="row">
             <div className="col-md-1 p-0 m-0"></div>
             <div className="col-md-8 p-0 mt-4">
-              <h2 className="text-center mb-4">Web Design Community Blogs</h2>
+              <h2 className="text-center mb-4">WebDesign Community Blogs</h2>
               <Link to="/home" className="badge badge-primary mr-1">
                 Home >
               </Link>
               <Link to="/index_articles" className="badge badge-warning mr-1">
                 Blogs >
               </Link>
+
               <div className="card-deck m-2">
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Programming Languages Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/prog_lan_blog"
+                <ArticlePostCard
+                  imgsource="js.jpeg"
+                  tag1="Node js"
+                  tag2="api"
+                  title="How to Use SSL/TLS with Node.js"
+                  paragraph="In 2020, there’s no reason for your website not to use HTTPS. Visitors expect it, Google uses it as a ranking factor and browser makers will happily name and shame those sites not using it."
+                  link="https://www.sitepoint.com/how-to-use-ssltls-with-node-js/"
                 />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
+                <ArticlePostCard
+                  imgsource="js.jpeg"
+                  tag1="javasript"
+                  tag2="nodejs"
+                  title="Five Ways to Lazy Load Images for Better Website Performance"
+                  paragraph="With images being among the most popular type of content on the web, page load time on websites can easily become an issue."
+                  link="https://www.sitepoint.com/five-techniques-lazy-load-images-website-performance/?utm_source=rss"
                 />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
+                <ArticlePostCard
+                  imgsource="js.jpeg"
+                  tag1=""
+                  tag2=""
+                  title=""
+                  paragraph=""
+                  link=""
                 />
               </div>
-              <div className="card-deck m-2">
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Programming Languages Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/prog_lan_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
               </div>
-              <div className="card-deck m-2">
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Programming Languages Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/prog_lan_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
-                <PostCard
-                  imgsource="programming_boooks.jpeg"
-                  title="Web Design Blog"
-                  paragraph="We Love to Code and this is the best way to express our
-                  love to Computer Science Wolrd."
-                  link="/webdesign_blog"
-                />
-              </div>
-            </div>
             <div className="col-md-3 ">
               <RightSideSection />
-            </div>
+              </div>
           </div>
         </div>
       </>
