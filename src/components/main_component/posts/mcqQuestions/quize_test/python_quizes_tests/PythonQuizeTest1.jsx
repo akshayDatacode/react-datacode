ca10d7764f7ea2dc3c03e202a18b1bf0ec96d438
dhanwebllisto <dhanshreewebllisto01@gmail.com>
2020-04-16 23:05:58 +0530

    quiz added

--- src/components/main_component/posts/mcqQuestions/quize_test/python_quizes_tests/PythonQuizeTest1.jsx
+++ src/components/main_component/posts/mcqQuestions/quize_test/python_quizes_tests/PythonQuizeTest1.jsx
@@ -52,44 +52,124 @@ class PythonQuizeTest1 extends Component {
             <div className="col-md-7 ">
               <QuizeTest
                 quizeNo="1"
-                question="Which of the following is not a keyword?"
-                correctAnswer="Programming"
-                v1="eval"
-                v2="assert"
-                v3=" nonlocal"
-                v4="pass"
+                question="In Python, what is one function to output content to the console?"
+                correctAnswer="print"
+                v1=" echo
+                "
+                v2=" output
+                "
+                v3=" print
+                "
+                v4=" console.log
+                "
                 counter={this.counter}
               />
+
               <QuizeTest
                 quizeNo="2"
-                question="Which of the following is an invalid statement?"
-                correctAnswer="a b c = 1000 2000 3000"
-                v1="abc = 1,000,000"
-                v2="a b c = 1000 2000 3000"
-                v3="a,b,c = 1000, 2000, 3000"
-                v4="a_b_c = 1,000,000"
+                question="Which of the following statements is NOT true about Python?"
+                correctAnswer="Python’s syntax is much like PHP"
+                v1="Python’s syntax is much like PHP"
+                v2=" Python can be used for web development"
+                v3="Python can run on any type of platform"
+                v4="Python can be used to generate dynamic web pages"
                 counter={this.counter}
               />
+              
               <QuizeTest
                 quizeNo="3"
-                question="Which of the following will run without errors?"
-                correctAnswer="round(45.8)"
-                v1="round(45.8)"
-                v2="round(6352.898,2,5)"
-                v3="round()"
-                v4="round(7463.123,2,1)"
+                question="What symbol can you use to comment out one line of code?"
+                correctAnswer="#"
+                v1="*"
+                v2="(comment)"
+                v3="//"
+                v4="#"
                 counter={this.counter}
               />
+              
               <QuizeTest
                 quizeNo="4"
-                question="Which of the following is incorrect?"
-                correctAnswer="float(’12+34′)"
-                v1="float(‘inf’)"
-                v2="float(‘nan’)"
-                v3="float(’56’+’78’)"
-                v4="float(’12+34′)"
+                question="How do you create a variable “a” that is equal to 2?"
+                correctAnswer="a = 2"
+                v1="var a = 2"
+                v2=" int a = 2"
+                v3="a = 2"
+                v4="variable a = 2"
                 counter={this.counter}
               />
+                  <QuizeTest
+                quizeNo="5"
+                question=" How would you cast the string variable “a” that is equal to “2” into the integer 2?"
+                correctAnswer="int(a)
+                "
+                v1="castToInt(a)"
+                v2="int(a)"
+                v3="integer(a)"
+                v4="castToInteger(a)"
+                counter={this.counter}
+              />
+                  <QuizeTest
+                quizeNo="6"
+                question="Which of the following is not a valid assignment operator?"
+                correctAnswer="X="
+                v1="+="
+                v2=" -="
+                v3=" *="
+                v4="X="
+                counter={this.counter}
+              />
+              
+              <QuizeTest
+                quizeNo="7"
+                question="Which one of the following is a valid Python if statement"
+                correctAnswer="if a >= 22:
+                "
+                v1="if a >= 22:"
+                v2="if (a >= 22)"
+                v3="if (a => 22)"
+                v4="if a >= 22"
+                counter={this.counter}
+              />
+              
+              <QuizeTest
+                quizeNo="8"
+                question="What keyword would you use to add an alternative condition to an if statement?"
+                correctAnswer="elif"
+                v1="else if"
+                v2="elseif"
+                v3="elif"
+                v4=" None of the above"
+                counter={this.counter}
+              />
+              
+              <QuizeTest
+                quizeNo="9"
+                question="Which of the following is a valid way to start a function in Python?"
+                correctAnswer="def someFunction():"
+                v1="def someFunction():"
+                v2="function someFunction()
+                "
+                v3="def someFunction()
+                "
+                v4="function someFunction():"
+                counter={this.counter}
+              />
+              
+              <QuizeTest
+                quizeNo="10"
+                question="Which of the following is a valid for loop in Python?"
+                correctAnswer="for a in range(1,3):"
+                v1="for(a = 0; a < 3; a++)"
+                v2=" for a in range(3)"
+                v3=" for a loop 3:
+                "
+                v4="for a in range(1,3):"
+                counter={this.counter}
+              />
+              
+              
+              
+             
             </div>
           </div>
         </div>
