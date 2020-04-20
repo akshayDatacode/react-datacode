1f97f2f560d9ed5b194b7a4ca40d3084d73ecf46
dhanwebllisto <dhanshreewebllisto01@gmail.com>
2020-04-20 17:18:12 +0530

    Design Changes

--- src/components/main_component/posts/mcqQuestions/angular_mcqs/AngularMCQ1.jsx
+++ src/components/main_component/posts/mcqQuestions/angular_mcqs/AngularMCQ1.jsx
@@ -37,37 +37,144 @@ class AngularMCQ1 extends Component {
                   <FontAwesomeIcon icon={faHome} size="1x" />
                 </Link>
                 <Link to="/index_mcqs" className="badge badge-warning mr-1">
-                  MCQs >{" "}
+                  MCQs >>
                 </Link>
                 <Link
                   to="/index_angular_mcqs"
                   className="badge badge-success mr-1"
                 >
-                  Angular MCQs >
+                  Angular MCQs >>
                 </Link>
                 <ScoreCard count={this.state.count} />
               </div>
               <div className="row">
-                <div className="col"></div>
+                <div className="col">
+                <Quize
+                    quizeNo="1"
+                    question="Q.1 Which community Angular JS belongs to?
+                    "
+                    correctAnswer="Google
+                    "
+                    v1="Twitter"
+                    v2="Google
+                    "
+                    v3="Microsoft
+                    "
+                    v4="Facebook"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="2"
+                    question="Q.2 AngularJS is entirely based on HTML and JavaScript
+                    "
+                    correctAnswer="True "
+                    v1="True"
+                    v2="False"
+                    v3="Maybe"
+                    v4="Not sure"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="3"
+                    question="Q.3 What is the correct syntax to write an expression?
+                    "
+                    correctAnswer="{{expression}} "
+                    v1="{{expression}}"
+                    v2="[expression]"
+                    v3="{expression}"
+                    v4="[[expression]]"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="4"
+                    question="Q.4 Which of the following can be used as a prefix for Directive?"
+                    correctAnswer="All of the above "
+                    v1="ng-"
+                    v2="x-"
+                    v3="data-"
+                    v4="All of the above"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="5"
+                    question="Q.5 Which directive is used to start an angularJS application?"
+                    correctAnswer="ng-app "
+                    v1="ng-start"
+                    v2="ng-app"
+                    v3="ng-begin"
+                    v4="ng-init"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="6"
+                    question="Q.6 Which directive binds the values of application data to HTML input controls in angular JS?"
+                    correctAnswer="ng-model "
+                    v1="ng-bind"
+                    v2="None of the above"
+                    v3="ng-model"
+                    v4="ng-app"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="7"
+                    question="Q.7 Which directive binds Application data to HTML tags in angular JS?"
+                    correctAnswer="ng-bind "
+                    v1="ng-app"
+                    v2="ng-model"
+                    v3="ng-bind"
+                    v4="None of the above"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="8"
+                    question="Q.8 Can we create custom directive in Angular JS?"
+                    correctAnswer="True "
+                    v1="True"
+                    v2="False"
+                    v3="Maybe"
+                    v4="Not sure"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="9"
+                    question="Q.9 Which of the following is a filter in Angular Js?"
+                    correctAnswer="All of the above "
+                    v1="Currency"
+                    v2="Date"
+                    v3="Uppercase"
+                    v4="All of the above"
+                    counter={this.counter}
+                  />
+                    <Quize
+                    quizeNo="10"
+                    question="Q.10 How to combine filter with expression?"
+                    correctAnswer="Using PIpe {{expression | pipe}} "
+                    v1="Using Comma {{expression, pipe}}"
+                    v2="Using Dot {{expression. pipe}}"
+                    v3="Using PIpe {{expression | pipe}}"
+                    v4="Using Slash {{expression / pipe}}"
+                    counter={this.counter}
+                  />
+                </div>
               </div>
             </div>
             <div className="col-md-4">
               <RightSideSection />
               <h5 className="mb-3">Recommended for you</h5>
               <RecommendPost
-                imgsource="python_mcqs.jpeg"
-                RecPostHading="Python MCQs Questions"
-                link="/index_python_mcqs"
+                imgsource="html_mcq.jpeg"
+                RecPostHading="HTML MCQs Questions"
+                link="/index_html_mcqs"
               />
               <RecommendPost
-                imgsource="java_mcqs.jpeg"
-                RecPostHading="Java MCQs Questions"
+                imgsource="js_mcq.jpeg"
+                RecPostHading="JavaScript MCQs Questions"
                 link="/index_java_mcqs"
               />
               <RecommendPost
-                imgsource="datacodelogo.jpeg"
-                RecPostHading="Programminig Quize Test"
-                link="/index_quize_tests"
+                imgsource="react_mcq.jpeg"
+                RecPostHading="React MCQs Questions"
+                link="/index_react_mcqs"
               />
             </div>
           </div>
