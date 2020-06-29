e626526e96ee2f41d37a6aa4b6b951c67150c569
miss-shree <dhanshreenirgude80@gmail.com>
2020-06-30 00:56:23 +0530

    videos added

--- src/components/main_component/video/IndexVideo.jsx
+++ src/components/main_component/video/IndexVideo.jsx
@@ -13,9 +13,45 @@ class IndexVideo extends Component {
         <div id="video-top">
       <h1> Top and latest webinars</h1>
       </div>
+        
         <div className="row">
           <div className="col">
-            <VideoRow videoId="Ewa4-URpMg0" />
+            <VideoRow videoId="UYxTC3Y4Lwc" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="3ajFpYW-Qd4" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="e2l3v6QbJPk" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="H3vqbhvQcx4" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="o4wdgzSm3gQ" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="dIrAnD5tWEQ" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="7T5Y6V7jlYU" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="lYANvzJ4HtM" />
           </div>
         </div>
         <div className="row">
@@ -25,27 +61,37 @@ class IndexVideo extends Component {
         </div>
         <div className="row">
           <div className="col">
-            <VideoRow videoId="Ewa4-URpMg0" />
+            <VideoRow videoId="eFOFKkqroYk" />
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <VideoRow videoId="Ewa4-URpMg0" />
+            <VideoRow videoId="74t29bYS0kjM" />
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <VideoRow videoId="Ewa4-URpMg0" />
+            <VideoRow videoId="K0Dy70Q4zS4" />
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <VideoRow videoId="Ewa4-URpMg0" />
+            <VideoRow videoId="UdEzRTOaUys" />
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <VideoRow videoId="Ewa4-URpMg0" />
+            <VideoRow videoId="33i4pcA-HAw" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="rVg-XLWE4_o" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <VideoRow videoId="HuklCMnc3Ao" />
           </div>
         </div>
       </>
