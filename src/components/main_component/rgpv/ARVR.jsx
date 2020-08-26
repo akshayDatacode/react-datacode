270fe1ac0f177071bf8fbd38d5ecb70576b13861
miss-shree <dhanshreenirgude80@gmail.com>
2020-08-26 20:43:11 +0530

    AR & VR MCQs added

--- src/components/main_component/rgpv/ARVR.jsx
+++ src/components/main_component/rgpv/ARVR.jsx
@@ -47,10 +47,70 @@ class ARVR extends Component {
             </Link>
             <RoadMapCard
               imgsource="programming_blog.jpeg"
-              title="Updatinig Soon!!"
-              tag1="RGPV Exam MCQs"
-              link="datacode.in"
+              title="Virtual Reality - Computer Fundamentals Questions and Answers
+              "
+              source="(sanfoundry.com)"
+              tag1="MCQs"
+              link="https://www.sanfoundry.com/computer-fundamentals-questions-answers-virtual-reality/"
             />
+            <RoadMapCard
+              imgsource="programming_blog.jpeg"
+              title="Augmented Reality Quiz"
+              source="(quizizz.com)"
+              tag1="MCQs"
+              link="https://quizizz.com/admin/quiz/5dd33bb75d5cc0001c84580c/augmented-reality-quiz"
+            />
+            <RoadMapCard
+              imgsource="programming_blog.jpeg"
+              title="AR and VR"
+              source="(searchmobilecomputing.techtarget.com
+                )"
+              tag1="MCQs"
+              link="https://searchmobilecomputing.techtarget.com/quiz/Quiz-Are-you-ready-for-AR-and-VR-in-the-enterprise"
+            />
+            <RoadMapCard
+              imgsource="programming_blog.jpeg"
+              title="AR Quiz: How much do you really know about augmented reality? "
+              source="(blippar.com
+                )"
+              tag1="MCQs"
+              link="https://www.blippar.com/blog/2018/03/22/quiz-how-much-do-you-really-know-about-augmented-reality"
+            />
+            <RoadMapCard
+              imgsource="programming_blog.jpeg"
+              title="Quiz / Worksheet - Augmented Reality in the Classroom
+              "
+              source="(study.com
+
+                )"
+              tag1="MCQs"
+              link="https://study.com/academy/practice/quiz-worksheet-augmented-reality-in-the-classroom.html"
+            />
+            <RoadMapCard
+              imgsource="programming_blog.jpeg"
+              title="Augmented-reality-Virtual-reality quiz
+              "
+              source="(objectivequiz.com)"
+              tag1="MCQs"
+              link="https://www.objectivequiz.com/objective-questions/computer-science/augmented-reality-virtual-reality"
+            />
+            <RoadMapCard
+              imgsource="programming_blog.jpeg"
+              title="Augmented Reality(Ar) And Virtual Reality (Vr) Quiz
+              "
+              source="(proprofs.com
+                )"
+              tag1="MCQs"
+              link="https://www.proprofs.com/quiz-school/story.php?title=3dq-augmented-realityar-and-virtual-reality-vr-quiz"
+            />
+            <RoadMapCard
+              imgsource="programming_blog.jpeg"
+              title="Computer Fundamentals MCQs"
+              source="(sanfoundry.com)"
+              tag1="MCQs"
+              link="https://www.sanfoundry.com/computer-fundamentals-questions-answers-mcqs/"
+            />
+            
           </div>
 
           <div className="col-md-4 mt-md-5 mt-3">
