a1a16ed037b0521359472497df29b2c1b02f6c81
Mukesh Patel <mukesh@runmybusiness.com>
2020-09-15 22:44:51 +0530

    New header and Slider Header Integrated

--- src/components/header_component/HeaderComponent.jsx
+++ src/components/header_component/HeaderComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "../../assets/css/ToolBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   fab,
@@ -32,10 +33,14 @@ import {
   faVideoSlash,
   faVideo,
   faUniversity,
+  faInfoCircle,
+  faSplotch,
+  faComments,
 } from "@fortawesome/free-solid-svg-icons";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import fire from "../../config/fire";
+import DrawerToggleButton from "./side_drawer/DrawerToggleButton";
 
 class HeaderComponent extends Component {
   state = {};
@@ -47,147 +52,90 @@ class HeaderComponent extends Component {
   render() {
     return (
       <>
-        <Navbar
-          collapseOnSelect
-          fixed="top"
-          expand="lg"
-          variant="dark"
-          className="m-0 p-0"
-          style={{ backgroundColor: "#264767" }}
-        >
-          <Navbar.Brand
-            collapseOnSelect="false"
-            aria-controls="responsive-navbar-nav"
-          >
-            <Nav.Link
-              href="/react-datacode/#/"
-              className="text-white h3 p-0 ml-3 mr-0 mt-0 mb-0 "
-            >
-              Datacode.in
-            </Nav.Link>
-          </Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="responsive-navbar-nav"
-            className="mr-1 border-0"
-          />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto ml-2">
-              <Nav.Link
-                href="/react-datacode/#/home"
-                style={{ color: "#04FFD5" }}
-              >
-                <FontAwesomeIcon icon={faHome} /> Home
-              </Nav.Link>
-              <NavDropdown
-                title={
-                  <span className="my-auto" style={{ color: "#04FFD5" }}>
-                    <FontAwesomeIcon icon={faBookOpen} /> Learn
+        <header className="toolbar fixed-top">
+          <nav className="toolbar_navigation ">
+            <div className="toolbar_toggle-button">
+              <DrawerToggleButton clickButton={this.props.drawerClickHandler} />
+            </div>
+            <div className="toolbar_logo">
+              <a href="/react-datacode/#/"><h2>Datacode.in</h2></a>
+            </div>
+            {/* This is for Logo and Nev Spaccing */}
+            <div className="logo-nev-spacer" />
+            <div className="toolbar_navigation-items">
+              <ul>
+                <li>
+                  <a href="/react-datacode/#/home">
+                    <FontAwesomeIcon icon={faHome} className="mr-2" />
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <NavDropdown
+                    title={
+                      <span className="my-auto" style={{ color: "#04FFD5" }}>
+                        <FontAwesomeIcon icon={faBookOpen} /> Learn
                   </span>
-                }
-                id="collasible-nav-dropdown"
-              >
-                <NavDropdown.Item href="/react-datacode/#/index_technologies">
-                  <FontAwesomeIcon
-                    icon={faChalkboardTeacher}
-                    className="mr-2"
-                  />
-                  Tutorials & Courses
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/react-datacode/#/index_training_certification">
-                  <FontAwesomeIcon icon={faAward} className="mr-2" />
-                  Training & Certification
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/react-datacode/#/index_mcqs">
-                  <FontAwesomeIcon icon={faTasks} className="mr-2" />
-                  Programming MCQs
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/react-datacode/#/index_code_challange">
-                  <FontAwesomeIcon icon={faLaptopCode} className="mr-2" />
-                  Code Challange
-                </NavDropdown.Item>
+                    }
+                    id="collasible-nav-dropdown"
+                  >
+                    <NavDropdown.Item href="/react-datacode/#/index_technologies">
+                      <FontAwesomeIcon
+                        icon={faChalkboardTeacher}
+                        className="mr-2"
+                      />
+                    Tutorials & Courses
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/react-datacode/#/index_training_certification">
+                      <FontAwesomeIcon icon={faAward} className="mr-2" />
+                      Training & Certification
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/react-datacode/#/index_mcqs">
+                      <FontAwesomeIcon icon={faTasks} className="mr-2" />
+                      Programming MCQs
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/react-datacode/#/index_code_challange">
+                      <FontAwesomeIcon icon={faLaptopCode} className="mr-2" />
+                      Code Challange
+                    </NavDropdown.Item>
 
-                <NavDropdown.Item href="/react-datacode/#/index_interview_questioins">
-                  <FontAwesomeIcon icon={faUserTie} className="mr-2" />
-                  Interview Preparation
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/react-datacode/#/index_practice_questions">
-                  <FontAwesomeIcon icon={faCode} className="mr-2" />
-                  Programming Practice Questions
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/react-datacode/#/index_quize_tests">
-                  <FontAwesomeIcon icon={faFlask} className="mr-2" />
-                  Programming Quiz Tests
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/react-datacode/#/index_programming_questions">
-                  <FontAwesomeIcon icon={faFileCode} className="mr-2" />
-                  Programming Example Questions
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link
-                href="/react-datacode/#/index_articles"
-                style={{ color: "#04FFD5" }}
-              >
-                <FontAwesomeIcon icon={faNewspaper} /> Blogs
-              </Nav.Link>
-              <Nav.Link
-                href="/react-datacode/#/index_events"
-                style={{ color: "#04FFD5" }}
-              >
-                <FontAwesomeIcon icon={faClipboardList} /> Events/Contests
-              </Nav.Link>
-              {/* <Nav.Link
-                href="/react-datacode/#/index_video"
-                style={{ color: "#04FFD5" }}
-              >
-                <FontAwesomeIcon icon={faVideo} /> Video
-              </Nav.Link> */}
-              <Nav.Link
-                href="/react-datacode/#/rgpv"
-                style={{ color: "#04FFD5" }}
-              >
-                <FontAwesomeIcon icon={faUniversity} /> RGPV MCQs Questions
-              </Nav.Link>
-            </Nav>
-
-            <NavDropdown.Divider />
-            <Nav>
-              <Nav.Item className="ml-2 ">
-                <a
-                  href="https://www.facebook.com/Datacode.in"
-                  className="text-white mr-3"
-                  target="_blank"
-                >
-                  <FontAwesomeIcon icon={faFacebook} size="2x" />
-                </a>
-                <a
-                  href="https://www.instagram.com/datacode.in/"
-                  className="text-white mr-3"
-                  target="_blank"
-                >
-                  <FontAwesomeIcon icon={faInstagram} size="2x" />
-                </a>
-                <a
-                  href="https://www.facebook.com/Datacode.in"
-                  className="text-white mr-3"
-                  target="_blank"
-                >
-                  <FontAwesomeIcon icon={faTwitter} size="2x" />
-                </a>
-              </Nav.Item>
-              {/* {this.props.isUserLogin ? (
-                <Nav.Item style={{ color: "#FF5733" }} onClick={this.logout}>
-                  <FontAwesomeIcon
-                    icon={faSignOutAlt}
-                    size="2x"
-                    className="mr-3"
-                  />
-                </Nav.Item>
-              ) : (
-                " "
-              )} */}
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+                    <NavDropdown.Item href="/react-datacode/#/index_interview_questioins">
+                      <FontAwesomeIcon icon={faUserTie} className="mr-2" />
+                      Interview Preparation
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/react-datacode/#/index_practice_questions">
+                      <FontAwesomeIcon icon={faCode} className="mr-2" />
+                      Programming Practice Questions
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/react-datacode/#/index_quize_tests">
+                      <FontAwesomeIcon icon={faFlask} className="mr-2" />
+                      Programming Quiz Tests
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/react-datacode/#/index_programming_questions">
+                      <FontAwesomeIcon icon={faFileCode} className="mr-2" />
+                      Programming Example Questions
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </li>
+                <li>
+                  <a href="/react-datacode/#/index_articles">
+                    <FontAwesomeIcon icon={faNewspaper} /> Blogs
+                  </a>
+                </li>
+                <li>
+                  <a href="/react-datacode/#/index_events">
+                    <FontAwesomeIcon icon={faClipboardList} /> Events/Contests
+                  </a>
+                </li>
+                {/* <li>
+                  <a href="#footer" onClick={this.logout}>
+                    Log Out
+                  </a>
+                </li> */}
+              </ul>
+            </div>
+          </nav>
+        </header>
       </>
     );
   }
--- src/components/header_component/IndexHeader.jsx
+++ src/components/header_component/IndexHeader.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
 import HeaderComponent from "./HeaderComponent";
+import SideDrawer from "./side_drawer/SideDrawer";
+import BackDrop from "./back_drop/BackDrop";
+
 class IndexHeader extends Component {
-  state = {};
+  state = {
+    sideDrawerOpen: false,
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backDropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
   render() {
+    let backDrop;
+
+    if (this.state.sideDrawerOpen) {
+      backDrop = <BackDrop click={this.backDropClickHandler} />;
+    }
     return (
       <>
-        <HeaderComponent isUserLogin={this.props.isUserLogin} />
+        <HeaderComponent
+          drawerClickHandler={this.drawerToggleClickHandler}
+          isUserLogin={this.props.isUserLogin}
+        />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backDrop}
       </>
     );
   }
